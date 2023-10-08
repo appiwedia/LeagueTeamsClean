e737d15405bff38650cfd878b88275a086ddb1f5
Mickaël MAS <appiwedia@gmail.com>
2023-10-09 00:05:04 +0200

    Quality: Add documentation on LeagueRequest and TeamRequest

--- LeagueTeamsCleanNetworking/Sources/LeagueTeamsCleanNetworking/Fetchers/Leagues/LeaguesRequest.swift
+++ LeagueTeamsCleanNetworking/Sources/LeagueTeamsCleanNetworking/Fetchers/Leagues/LeaguesRequest.swift
@@ -1,11 +1,19 @@
 import Foundation
 
+/// Represents API requests related to leagues.
+///
+/// Conforms to `RequestProtocol` to define details of the league-related requests.
 public enum LeaguesRequest: RequestProtocol {
+    
+    /// Represents a request to fetch all available leagues.
     case allLeagues
 
+    /// The path for the API endpoint related to the request.
     public var path: String {
         "/api/v1/json/50130162/all_leagues.php"
-    }   
+    }
 
+    /// The HTTP method associated with the request.
     public var requestType: RequestType { .GET }
 }
+
--- LeagueTeamsCleanNetworking/Sources/LeagueTeamsCleanNetworking/Fetchers/Teams/TeamsRequest.swift
+++ LeagueTeamsCleanNetworking/Sources/LeagueTeamsCleanNetworking/Fetchers/Teams/TeamsRequest.swift
@@ -1,12 +1,23 @@
 import Foundation
 
+/// Represents API requests related to teams.
+///
+/// Conforms to `RequestProtocol` to define details of the team-related requests.
 public enum TeamsRequest: RequestProtocol {
+    
+    /// Represents a request to fetch all teams for a given league by its name.
+    ///
+    /// - Parameter name: The name of the league for which teams need to be fetched.
     case getTeamsForLeague(name: String)
 
+    /// The path for the API endpoint related to the request.
     public var path: String {
         "/api/v1/json/50130162/search_all_teams.php"
     }
 
+    /// URL parameters to be included in the request.
+    ///
+    /// Depending on the request type, it defines the necessary parameters for the API call.
     public var urlParams: [String: String?] {
         switch self {
         case let .getTeamsForLeague(name):
@@ -14,5 +25,7 @@ public enum TeamsRequest: RequestProtocol {
         }
     }
 
+    /// The HTTP method associated with the request.
     public var requestType: RequestType { .GET }
 }
+
